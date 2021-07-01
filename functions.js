e5c1c8c22eb0d591e58571fd6ee9ad702f4bd1a4
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-07-01 20:24:09 +0200

    js_functions

--- functions.js
+++ functions.js
@@ -2,3 +2,101 @@
 //     console.log('Hello World');
 // }
 
+// ************************
+
+// function sum(a,b){
+//     console.log(a+b);
+// }
+
+// sum(12,3)
+
+// ************************
+
+// function sum(a,b){
+//     return (a+b);
+// }
+
+// sum(12,3) //sonucu göremedik return ü yakala
+
+// let toplam = sum(12,3)
+// console.log('Result: ', toplam);
+// console.log(sum(12,3)+5);
+
+// ************************
+
+// function sum(){
+//     // return ('hallo');
+//     console.log('hallo');
+// }
+// console.log(typeof sum());
+
+// ************************
+
+// function addTwo(number){
+//     return +number + 2  // number in önündeki '+' ile gelen stringi number yapiyoruz
+//    // Number(number)  type conversion
+// }
+
+// console.log(addTwo('6'));
+
+// ************************
+
+// function hallo (name = 'New User'){
+//     console.log(`Hallo ${name}`);
+// }
+
+// hallo('Victor')
+// hallo('Valery')
+// hallo()
+
+// ************************
+
+// function sayHi(myName){
+//     console.log(`Hallo ${myName}`);
+//     myName = 'Ed'
+//     console.log(`Hallo ${myName}`);
+// }
+
+// let myName = 'Mark';
+// sayHi(myName)
+// console.log('Outside function', myName);
+
+// ************************
+
+// let student = {};
+// student.name = 'Mark'
+
+// function sayHi2(student){
+//     console.log(`Hello ${student.name} entry point`);
+//     student.name = 'John';
+//     console.log(`Hello ${student.name} first point`);
+//     student = {name : 'Leon'};
+//     console.log(`Hello ${student.name} inside function`);
+// }
+
+// sayHi2(student)
+// console.log(`Hello! ${student.name} from outside`);
+
+// ************************
+
+// function div(num1, num2) {
+//     // console.log(num1/num2);
+//     if (num2 === 0) {
+//         console.log("Zero Division Error");
+//     } else {
+//         console.log(num1 / num2);
+//     }
+// }
+
+// div(12, 4)
+
+// ************************
+
+
+
+
+
+
+
+
+
